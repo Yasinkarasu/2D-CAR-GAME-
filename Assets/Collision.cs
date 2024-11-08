fab46c23da16492c70c7e286bafd04489ff9c2eb
Yasin Karasu <yasin.karasu5734@gmail.com>
2024-11-08 22:10:37 +0300

    Kamera Takip Eklendi

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,11 +5,15 @@ public class Collision : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) 
     {
+        
         Debug.Log("Vurdun");
     }
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         Debug.Log("Bu ne?");
+
     }
 }
+
+
